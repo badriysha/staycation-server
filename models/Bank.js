4490f4e7761664f5f5278952ca1694977efdb599
badriysha <badriysha@gmail.com>
2020-09-18 08:28:02 +0700

    Initial commit

---
+++ models/Bank.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const bankSchema = mongoose.Schema({
+  bankName: {
+    type: String,
+    required: true,
+  },
+  accountName: {
+    type: String,
+    required: true,
+  },
+  accountNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Bank", bankSchema);
